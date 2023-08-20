5240f59aafc8b5166c3108789d113efce6b8245a
apinanyogaratnam <apinan@ontopical.com>
2023-08-20 15:08:40 -0400

    feat: event function

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+interface Props {
+    event_name: string;
+    event_data: {
+        [key: string]: any;
+    }
+}
+
+const event = async (props: Props) => {
+    const getLocation = (): Promise<GeolocationPosition> => {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                (position) => resolve(position),
+                (error) => reject(error)
+            );
+        });
+    };
+
+    const location = await getLocation();
+
+    await axios.post("http://localhost:8000/analytics", {
+        data: JSON.stringify({
+            event_name: props.event_name,
+            event_data: props.event_data,
+            user_agent: navigator.userAgent,
+            vendor: navigator.vendor,
+            platform: navigator.platform,
+            language: navigator.language,
+            hardware_concurrency: navigator.hardwareConcurrency,
+            longitude: location.coords.longitude,
+            latitude: location.coords.latitude,
+            accuracy: location.coords.accuracy,
+            cookies_enabled: navigator.cookieEnabled,
+        }),
+    });
+};
+
+export { event };
